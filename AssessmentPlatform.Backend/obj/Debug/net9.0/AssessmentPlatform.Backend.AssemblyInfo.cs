e8e20b50a46a28a842586907d6eeaafe579a5f59
Kaveesha Kalhari <kaveeshahansani2003@gmail.com>
2025-04-29 11:45:13 +0530

    Add missing files

--- AssessmentPlatform.Backend/obj/Debug/net9.0/AssessmentPlatform.Backend.AssemblyInfo.cs
+++ AssessmentPlatform.Backend/obj/Debug/net9.0/AssessmentPlatform.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssessmentPlatform.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd750a975b6e2ca9c7b4f4b604bfc6ad6ad7acc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e446b223a30ae1d62106367648b1e5f24c74ff91")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssessmentPlatform.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssessmentPlatform.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
